519a32326a9950890db042bba2cc8ac35a1ba795
Jess Vu <jess.vu@slalom.com>
2026-05-20 12:23:27 -0400

    feat: add Vuetify type support for Vite

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+// Vuetify type support for Vite
+/// <reference types="vite/client" />
+declare module 'vuetify/styles';
+declare module 'vuetify';
+declare module 'vuetify/components';
+declare module 'vuetify/directives';
